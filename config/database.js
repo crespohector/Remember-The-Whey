8b04f9c604cd1c7f4d1f448adf43769a4bb37fbc
hector <crespomhector@gmail.com>
2024-06-04 21:56:39 -0400

    update connect-session-sequelize package to major version 6

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = {
     seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: 'postgres://ksjsemxfdayqwy:1f46325f673aa0bd58be6d10b6503b43b1c5d5da1b9e26fe2e4223d245924780@ec2-107-22-245-82.compute-1.amazonaws.com:5432/d4sp0snsimon9c',
+    use_env_variable: 'postgres://aa_projects_i4o5_user:ViTXnZ40eBvVTXqDcsNis6Yx8T0dEZZ6@dpg-cp00207jbltc73dlg8og-a/aa_projects_i4o5',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   }
